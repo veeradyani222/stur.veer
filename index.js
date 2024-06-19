870c3b73bc9e1c8a21b8fda3599b6d7600c5e6ee
veeradyani222 <167308256+veeradyani222@users.noreply.github.com>
2024-06-19 20:08:46 +0530

    commit js

---
+++ index.js
@@ -0,0 +1,60 @@
+
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        alert("Stur will be back with the Back-end soon!");
+    });
+});
+
+const links= document.querySelectorAll("a");
+
+
+links.forEach(links => {
+    links.addEventListener("click", function() {
+        alert("Stur will be back with the Back-end soon!");
+    });
+});
+
+const loginsignup= document.querySelectorAll(".loginsignup");
+
+
+loginsignup.forEach(loginsignup => {
+    loginsignup.addEventListener("click", function() {
+        alert("Stur will be back with the Back-end soon!");
+    });
+});
+
+const videos= document.querySelectorAll("video");
+
+
+videos.forEach(videos => {
+   videos.addEventListener("click", function() {
+        alert("Stur will be back with the Back-end soon!");
+    });
+});
+
+
+const icons= document.querySelectorAll("i");
+
+
+icons.forEach(icons => {
+   icons.addEventListener("click", function() {
+        alert("Stur will be back with the Back-end soon!");
+    });
+});
+
+
+const footer = document.querySelectorAll("footer ul li");
+
+
+footer.forEach( footer => {
+   footer.addEventListener("click", function() {
+        alert("Stur will be back with the Back-end soon!");
+    });
+});
+
+
+
+
